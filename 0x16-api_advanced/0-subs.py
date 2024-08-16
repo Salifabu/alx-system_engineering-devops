a4aecd5879c03c84668a8247d3cd7a38fef91041
Salifabu <otuemobeabubakar@gmail.com>
2024-08-15 21:33:20 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
@@ -13,5 +12,6 @@ def number_of_subscribers(subreddit):
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
-    return results.get("subscribers",0)
+    if results:
+        return results.get("subscribers", 0)
     return 0
